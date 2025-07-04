ba9cc1e4faf616bad1ccdace2adb003de48f6d2a
digitalmarketyasi <yasaman.r.206@gmail.com>
2025-07-04 17:09:19 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,29 @@
-
 import telebot
+from telebot import types
+import os
 
-BOT_TOKEN = "5225970776:AAGgVaLPicOps-in5IS7XamfPXp4oOufUEk"
+# دریافت توکن از متغیر محیطی
+TOKEN = os.getenv("BOT_TOKEN")
+bot = telebot.TeleBot(TOKEN)
 
-bot = telebot.TeleBot(BOT_TOKEN)
+# حذف وب‌هوک قبلی اگر وجود داشته باشه
+bot.remove_webhook()
 
+# هندلر برای دستور /start
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.send_message(message.chat.id, "سلام! به ربات خوش آمدید. 🎉")
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1 = types.KeyboardButton("🎁 شرکت در قرعه‌کشی")
+    item2 = types.KeyboardButton("📜 قوانین")
+    item3 = types.KeyboardButton("💰 موجودی من")
+    item4 = types.KeyboardButton("ℹ️ درباره ما")
 
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    bot.reply_to(message, "دستور شما: " + message.text)
+    markup.add(item1)
+    markup.add(item2, item3)
+    markup.add(item4)
 
-bot.polling()
+    bot.send_message(message.chat.id, "سلام! به ربات خوش آمدید 🎉", reply_markup=markup)
+
+# اجرای ربات
+print("ربات در حال اجراست...")
+bot.infinity_polling()
